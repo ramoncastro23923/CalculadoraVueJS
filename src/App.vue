15f10bd44ab5b384edade6d9ae3d84bf3af2e0f6
ramoncastro674 <tadeuramoncastro@gmail.com>
2024-06-26 13:44:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <div>
+      <input type="number" v-model.number="num1" placeholder="Número 1">
+      <select v-model="operation">
+        <option value="add">+</option>
+        <option value="subtract">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+      </select>
+      <input type="number" v-model.number="num2" placeholder="Número 2">
+    </div>
+    <h2>Resultado: {{ result }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operation: 'add'
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operation) {
+        case 'add':
+          return this.num1 + this.num2;
+        case 'subtract':
+          return this.num1 - this.num2;
+        case 'multiply':
+          return this.num1 * this.num2;
+        case 'divide':
+          return this.num2 !== 0 ? this.num1 / this.num2 : 'Erro: Divisão por zero';
+        default:
+          return 0;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input, select {
+  margin: 10px;
+  padding: 5px;
+}
+</style>
